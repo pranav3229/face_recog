9a9065e5bf5a68054e3e44d1eac4993369ac7132
pranav3229 <102801554+pranav3229@users.noreply.github.com>
2023-09-21 23:42:58 +0530

    Memory tracking

--- face_recog.py
+++ face_recog.py
@@ -5,6 +5,13 @@ import numpy as np
 from datetime import datetime
 import pickle
 
+
+from collections import Counter
+import linecache
+import os
+import tracemalloc
+
+
 path = 'student_images'
 
 images = []
@@ -38,18 +45,24 @@ def markAttendance(name):
             f.writelines(f'n{name}, {time}, {date}')
 
 # take pictures from webcam 
+tracemalloc.start()
 cap  = cv2.VideoCapture(0)
 while True:
+    
     success, img = cap.read()
     imgS = cv2.resize(img, (0,0), None, 0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
     faces_in_frame = face_recognition.face_locations(imgS)
     encoded_faces = face_recognition.face_encodings(imgS, faces_in_frame)
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Memory usage: {current / 10**6} MB (Peak: {peak / 10**6} MB)")
     for encode_face, faceloc in zip(encoded_faces,faces_in_frame):
         matches = face_recognition.compare_faces(encoded_face_train, encode_face)
         faceDist = face_recognition.face_distance(encoded_face_train, encode_face)
         matchIndex = np.argmin(faceDist)
         print(matchIndex)
+        current, peak = tracemalloc.get_traced_memory()
+        print(f"Memory usage: {current / 10**6} MB (Peak: {peak / 10**6} MB)")
         if matches[matchIndex]:
             name = classNames[matchIndex].upper().lower()
             y1,x2,y2,x1 = faceloc
@@ -58,7 +71,17 @@ while True:
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img, (x1,y2-35),(x2,y2), (0,255,0), cv2.FILLED)
             cv2.putText(img,name, (x1+6,y2-5), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            
             markAttendance(name)
+            
     cv2.imshow('webcam', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
+tracemalloc.stop()
+current, peak = tracemalloc.get_traced_memory()
+print(f"Memory usage: {current / 10**6} MB (Peak: {peak / 10**6} MB)")
+# Stop memory tracking
+tracemalloc.stop()
+
